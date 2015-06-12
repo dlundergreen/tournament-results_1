7f579b7727a7d7a050e07b9f2301a1c53b94868b
dlundergreen <dlundergreen@gmail.com>
2015-06-11 20:02:27 -0700

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -1,14 +1,12 @@
 -- Table definitions for the tournament project.
 --
--- First use the out the CREATE DATABASE CODE
--- Then use the CREATE TABLE code to create the table that is needed.
-
+-- Run this code using command 'psql  \g tournament.sql" to create the Database for this project.
 
 -- Step 1 Create Database:
 CREATE DATABASE tournament;
 
 -- Step 2 Create the needed table.
-
+\c tournament
 CREATE TABLE players
 (
   id serial,
